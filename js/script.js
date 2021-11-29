b4737ae4a11a979b543b7c9a3a5e1be8b6d9bf2b
Joel Silva <joel.pjds@gmail.com>
2021-11-29 13:46:49 -0300

    Criando a parte do Javascript

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
-const peso = document.getElementById("peso").value
-const altura = document.getElementById("altura").value
-const botao = document.getElementById("botao")
-
-function calc(a,b){
-  const imc = a / b*2
-  return imc
+const btn = document.getElementById("botao")
+let resul = document.getElementById("resul")
+let resulDetalhe = document.getElementById("resul-detalhe")
+let resulbloco = document.getElementById("resultado-final")
+function calcular() {
+  let peso = parseFloat(document.getElementById("peso").value)
+  let altura = parseFloat(document.getElementById("altura").value)
+  let imc = peso / (altura * altura)
+  let resultado = ""
+  if (imc < 18.5) {
+    resultado = "Infelizmente você está </strong>abaixo do seu peso ideal</strong>"
+  } else if (imc < 25) {
+    resultado = "Você está no seu <strong>peso ideal</strong> !"
+  } else if (imc < 30) {
+    resultado = "Você está um pouco <strong>acima do seu peso ideal!</strong>"
+  } else if (imc < 35) {
+    resultado = "Você está com <strong>Obesidade grau 1!</strong>"
+  } else if (imc < 40) {
+    resultado = "Você está com <strong>Obesidade grau 2!</strong>"
+  } else if (imc >= 40) {
+    resultado = "Você está com <strong>Obesidade grau 3 ou Obesidade Mórbida!</strong>"
+  }
+  resul.innerHTML = imc.toFixed(1)
+  resulDetalhe.innerHTML = resultado
+  resulbloco.style.display = "block"
 }
-console.log(peso, altura)
+btn.addEventListener("click", calcular)
+
+
